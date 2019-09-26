aa39fa31dbe039dca83d7edf2019f6224f571509
Zscoob <james.zobian@gmail.com>
2019-09-26 18:20:07 -0500

    server working?

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const mongoose = require('mongoose');
 const mongooseOptions = {
   useNewUrlParser:true,
   useCreateIndex: true,
+  useUnifiedTopology: true,
 };
 mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
 
-require('./src/app.js.js.js').start(process.env.PORT);
+require('./src/app').start(process.env.PORT);
